ed98a3eebe1d2f45997aa215958274474322a733
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2021-11-13 23:07:12 +0530

    Update s3.tf

--- terraform/config/factory_vpc/s3.tf
+++ terraform/config/factory_vpc/s3.tf
@@ -1,7 +1,8 @@
 resource "aws_s3_bucket" "s3-vpc-flow-logs" {
   
   ### s3 bucket name doesn't allow _ so we are using replace function to replace _ with -
-  bucket = "${replace(terraform.workspace, "_", "-")}-vpc-flow-logs24"
+  #bucket = "${replace(terraform.workspace, "_", "-")}-vpc-flow-logs24"
+  bucket = "eksfactory-vpc-flow-logs2"
   #region = "eu-west-1"
 
   lifecycle {
