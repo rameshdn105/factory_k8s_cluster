7a5421a85cbc3e5e75ea1d8b93a0061f9abca0a9
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2021-11-21 11:51:43 +0530

    Create vpc.tf

---
+++ terraform/config/db_cluster_vpc/vpc.tf
@@ -0,0 +1,23 @@
+ resource "aws_vpc" "vpc" {
+     cidr_block = var.vpc_cidr_block
+     enable_dns_hostnames = true
+
+     tags = {
+         Name = var.vpc_name
+         Environment = var.vpc_environment
+     }
+ }
+
+ resource "aws_internet_gateway" "ig"{
+     vpc_id =aws_vpc.vpc.id
+
+     tags ={
+         Name = var.vpc_name
+     }
+ }
+
+ resource "aws_route" "route"{
+     route_table_id = aws_vpc.vpc.default_route_table_id
+     destination_cidr_block = "0.0.0.0/0"
+     gateway_id = aws_internet_gateway.ig.id
+ }
