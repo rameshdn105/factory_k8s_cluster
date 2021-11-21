71751a408c05fac6e214de2c43afc54cb3c31271
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2021-11-21 12:37:48 +0530

    Create vpc.tf

---
+++ terraform/config/db_ec2_vpc/vpc.tf
@@ -0,0 +1,74 @@
+resource "aws_vpc" "vpc" {
+     cidr_block = var.vpc_cidr_block
+     enable_dns_hostnames = true
+
+     tags = {
+         Name = var.vpc_name
+         Environment = var.vpc_environment
+     }
+ }
+
+ resource "aws_internet_gateway" "ig"{
+     vpc_id =aws_vpc.vpc.id
+
+     tags ={
+         Name = var.vpc_name
+     }
+ }
+
+resource "aws_route_table" "db_on_ec2_rt_table" {
+    vpc_id = aws_vpc.vpc.id
+    tags = {
+        Name = "${var.vpc_name}_rt"
+    }
+}
+
+ resource "aws_route" "route"{
+     route_table_id = aws_route_table.db_on_ec2_rt_table.id
+     destination_cidr_block = "0.0.0.0/0"
+     gateway_id = aws_internet_gateway.ig.id
+ }
+
+resource "aws_eip" "nat_eip" {
+    tags = {
+        Name = "db_ec2_nat_ep"
+    }
+}
+
+# resource "aws_internet_gateway" "eks" {
+#     vpc_id = data.aws_vpc.eks_vpc.id
+#     tags = {
+#         Name = "eks"
+#     }
+# }
+
+resource "aws_nat_gateway" "natgw" {
+    allocation_id =aws_eip.nat_eip.id
+    subnet_id = aws_subnet.eks_subnet_private.id
+    tags = {
+        Name = "eu-west-1"
+    }
+}
+
+resource "aws_route_table_association" "private_rtas" {
+    subnet_id = aws_subnet.eks_subnet_private.id
+    route_table_id = aws_route_table.db_on_ec2_rt_table.id
+}
+
+
+
+
+resource "aws_route" "nat_internet" {
+    count = length(var.subnets)
+    route_table_id = aws_route_table.db_on_ec2_rt_table.id
+    destination_cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgw.id
+}
+
+ resource "aws_subnet" "eks_subnet_private" {
+    availability_zone = "eu-west-1b"
+    vpc_id   = aws_vpc.vpc.id
+    cidr_block = var.private_subnets_cidr
+    tags = {
+     Name = "ec2_db_ssubnet_private"
+}
