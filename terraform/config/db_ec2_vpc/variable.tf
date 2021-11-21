b0f672913867d70dd0360b40f90ec04fa2fedc6f
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2021-11-21 12:45:05 +0530

    Update variable.tf

--- terraform/config/db_ec2_vpc/variable.tf
+++ terraform/config/db_ec2_vpc/variable.tf
@@ -1,5 +1,6 @@
-variable "vpc_id" {
+variable "vpc_name" {
     type = string
+    default = "db_vpc_c2"
 }
 
 variable "subnets" {
@@ -13,4 +14,15 @@ variable "subnets_public" {
 variable "aws_region" {
     type = string
     default = "eu-west-1"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  default     = "10.56.0.0/16"
+}
+
+
+variable "private_subnets_cidr" {
+  type        = string
+  default     = "10.56.24.0/22"
 }
