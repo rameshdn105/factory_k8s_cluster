2092ae133cb91777323829941a0d5ac450a1c120
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2021-12-21 22:31:31 +0530

    Create mysql.tf

---
+++ terraform/config/CI_Mysql_SetUp/mysql.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "db_instance" {
+  count         = 1
+  instance_type = "t2.micro"
+  ami           = "ami-0943382e114f188e8"
+  key_name      = "BastionKey"
+  subnet_id     = "subnet-d94cafa0"
+  vpc_security_group_ids      = "sg-0a44108342b2e84d6"
+  associate_public_ip_address = true
+  
+  }
