e11a513052c9a16b446c6b62d0da376cbcae4be9
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2021-12-22 09:52:26 +0530

    Update mysql.tf

--- terraform/config/CI_Mysql_SetUp/mysql.tf
+++ terraform/config/CI_Mysql_SetUp/mysql.tf
@@ -10,6 +10,6 @@ resource "aws_instance" "db_instance" {
   }
 resource "null_resource" "example1" {
   provisioner "local-exec" {
-    command = "echo ${aws_instance.db_instance[0].public_ip} >> /ip_addr.txt"  
+    command = "sudo echo ${aws_instance.db_instance[0].public_ip} >> /ip_addr.txt"  
   }
 }
