e91aebadf26499f5769ddb2cf116f8d4e41d82dc
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2021-12-22 13:53:45 +0530

    Create install_mysql.sh

---
+++ terraform/config/CI_Mysql_SetUp/install_mysql.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+password=$1
+if [ "$(which mysql 2>/dev/null)" ]
+then
+    echo " Mysql is already installed "
+    exit
+else
+    echo -e "\033[92m<<<<<<<<<<<<<<<<<<<   Installing Mysql!!... >>>>>>>>>>>>>>>>>>>>>\033[0m\n\n"
+    sudo apt-get install mysql-server -y
+    echo -e "\033[34m<<<<<<<<<<<<<<<<<<<  Setting Up Root USER >>>>>>>>>>>>>>>>>>>>>\033[0m\n\n"
+    sudo mysql <<EOF
+    ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$password';
+    exit
+EOF
+echo -e "\033[31m<<<<<<<<<<<<<<<<<<<    Granting remote access >>>>>>>>>>>>>>>>>>>>>\033[0m\n\n"
+mysql --user=root --password=$1 <<EOF
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$password' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
+fi
+sudo sed -i '43 s/^/#/' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo service mysql restart
+
+echo -e "\033[31m<<<<<<<<<<<<<<<<<<<    Finished >>>>>>>>>>>>>>>>>>>>>\033[0m"
+
+
+
