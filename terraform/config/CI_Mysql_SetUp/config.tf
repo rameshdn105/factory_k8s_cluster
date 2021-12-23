7a99f9f4b56d6d98bffa4ece60016907d7fcbb38
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2021-12-24 01:05:44 +0530

    Update config.tf

--- terraform/config/CI_Mysql_SetUp/config.tf
+++ terraform/config/CI_Mysql_SetUp/config.tf
@@ -1,11 +1,9 @@
-#terraform {
-#  required_version = ">= 0.12"
-#
-#  backend "s3" {
-#    bucket ="terraform.state2"
-#    key = "ci_build"
-#    region = "eu-west-1"
-#    profile = "shyamjith"
-#  }
-#  
-#}
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    bucket ="terraform.state2"
+    key = "ci_build"
+    region = "eu-west-1"
+    profile = "shyamjith"
+  }  
+}
