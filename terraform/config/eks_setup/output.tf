877a240ae293b5f198089debf04dbcd7fe085dc1
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2022-01-31 23:12:57 +0530

    Update output.tf

--- terraform/config/eks_setup/output.tf
+++ terraform/config/eks_setup/output.tf
@@ -1,3 +1,5 @@
 output "auth-config" {
     value = "${module.eks_setup.config_map_aws_auth}"
 }
+# this will be used inside the  jenkinsfile -> https://github.com/Shyamjith06/Pipelines/blob/main/pipeline_scripts/eks.groovy
+#stage('Setting up the Cluster Access'
