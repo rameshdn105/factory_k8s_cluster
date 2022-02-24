e6c563a0b23356c05764b76fc7edee272f0961c5
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2022-02-24 21:17:37 +0530

    Update eks.tf

--- terraform/config/eks_setup/eks.tf
+++ terraform/config/eks_setup/eks.tf
@@ -2,6 +2,6 @@ module "eks_setup" {
     source = "../../modules/eks_setup"
     vpc_id = data.aws_vpc.vpc.id
     subnets = local.subnets[terraform.workspace]
-    cluster_name = "factory"
+    cluster_name = "${terraform.workspace}"
     subnets_public = local.subnets_public[terraform.workspace]
 }
