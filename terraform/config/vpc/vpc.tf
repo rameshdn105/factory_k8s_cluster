ffa4977c903802feb6b474234762b25b086b705c
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2022-08-15 23:25:46 +0530

    Update vpc.tf

--- terraform/config/vpc/vpc.tf
+++ terraform/config/vpc/vpc.tf
@@ -1,6 +1,6 @@
 module "module "vpc" {
     source = "../../modules/vpc"
-    vpc_cidr_block = "10.55.0.0"
-    vpc_name = "dev"
-    vpc_environment = "dev"
+    vpc_cidr_block = local.vpc_cidr[terraform.workspace]
+    vpc_name = local.vpc_name[terraform.workspace]
+    vpc_environment = local.vpc_environment[terraform.workspace]
 }
